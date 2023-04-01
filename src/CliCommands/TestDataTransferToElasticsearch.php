92f524fa8ed0598573e3ab80139f40c75c4b6653
Eligijus Stugys <eligijus.stugys@gmail.com>
2023-04-01 17:29:53 +0300

    Connect

--- src/CliCommands/TestDataTransferToElasticsearch.php
+++ src/CliCommands/TestDataTransferToElasticsearch.php
@@ -6,7 +6,9 @@ use Elastic\Elasticsearch\Client;
 use Elastic\Elasticsearch\ClientBuilder;
 use Elastic\Elasticsearch\Exception\AuthenticationException;
 use Elastic\Elasticsearch\Exception\ClientResponseException;
+use Elastic\Elasticsearch\Exception\MissingParameterException;
 use Elastic\Elasticsearch\Exception\ServerResponseException;
+use Exception;
 use Log\Writer;
 
 class TestDataTransferToElasticsearch implements CliCommandInterface
@@ -26,6 +28,7 @@ class TestDataTransferToElasticsearch implements CliCommandInterface
         $this->env = $env;
 
         $this->elasticClient = ClientBuilder::create()
+            //->setSSLVerification(false)
             ->setElasticCloudId($this->env['ELASTIC_CLOUD_WEB_UI_CLOUD_ID'])
             ->setApiKey($this->env['ELASTIC_CLOUD_WEB_UI_API_KEY'])
             ->build();
@@ -34,12 +37,19 @@ class TestDataTransferToElasticsearch implements CliCommandInterface
     /**
      * @param array $arguments
      * @return void
+     * @throws MissingParameterException
      */
     public function execute(array $arguments): void
     {
         $this->logWriter->debugMessage("'execute' method started.");
 
         $this->printInfo();
+
+        $this->addIndexMyIndex();
+
+        $this->searchIndexInMyIndex();
+
+        $this->deleteDocumentInMyIndex();
 
         $this->logWriter->debugMessage("'execute' method finished.");
     }
@@ -51,12 +61,104 @@ class TestDataTransferToElasticsearch implements CliCommandInterface
     {
         try {
             $response = $this->elasticClient->info();
-            $this->logWriter->debugMessage("Status code: {$response->getStatusCode()}");
+            $this->logWriter->infoMessage("Status code: {$response->getStatusCode()}");
+            $this->logWriter->infoMessage("Version number: " . $response['version']['number']);
         } catch (ClientResponseException $e) {
             $this->logWriter->errorMessage("ClientResponseException: {$e->getMessage()}");
             exit(1);
         } catch (ServerResponseException $e) {
             $this->logWriter->errorMessage("ServerResponseException: {$e->getMessage()}");
+            exit(1);
+        }
+    }
+
+    /**
+     * @return void
+     */
+    private function addIndexMyIndex(): void
+    {
+        $params = [
+            'index' => 'my_index',
+            'id' => 'my_id',
+            'body' => ['testField' => 'abc']
+        ];
+
+        try {
+            $response = $this->elasticClient->index($params);
+            $this->logWriter->infoMessage("Index 'my_index' added.");
+        } catch (ClientResponseException $e) {
+            $this->logWriter->errorMessage("ClientResponseException: {$e->getMessage()}");
+            exit(1);
+        } catch (ServerResponseException $e) {
+            $this->logWriter->errorMessage("ServerResponseException: {$e->getMessage()}");
+            exit(1);
+        } catch (Exception $e) {
+            $this->logWriter->errorMessage("Exception: {$e->getMessage()}");
+            exit(1);
+        }
+    }
+
+    /**
+     * @return void
+     */
+    private function searchIndexInMyIndex(): void
+    {
+        $params = [
+            'index' => 'my_index',
+            'body' => [
+                'query' => [
+                    'match' => [
+                        'testField' => 'abc'
+                    ]
+                ]
+            ]
+        ];
+
+        try {
+            $response = $this->elasticClient->search($params);
+            $this->logWriter->infoMessage("Total docs: " . $response['hits']['total']['value']);
+            $this->logWriter->infoMessage("Max score: " . $response['hits']['max_score']);
+            $this->logWriter->infoMessage("Took: " . $response['hits']['max_score'] . " ms");
+            //$this->logWriter->infoMessage("Hits: " . print_r($response['hits']['hits'], true));
+        } catch (ClientResponseException $e) {
+            $this->logWriter->errorMessage("ClientResponseException: {$e->getMessage()}");
+            exit(1);
+        } catch (ServerResponseException $e) {
+            $this->logWriter->errorMessage("ServerResponseException: {$e->getMessage()}");
+            exit(1);
+        } catch (Exception $e) {
+            $this->logWriter->errorMessage("Exception: {$e->getMessage()}");
+            exit(1);
+        }
+    }
+
+    /**
+     * @return void
+     */
+    private function deleteDocumentInMyIndex(): void
+    {
+        try {
+            $response = $this->elasticClient->delete([
+                'index' => 'my_index',
+                'id' => 'my_id'
+            ]);
+            $this->logWriter->infoMessage("Document id 'my_id' removed from index 'my_index'.");
+            //print_r($response);
+//            if ($response['acknowledge'] === 1) {
+//                $this->logWriter->infoMessage("The document with id 'my_id' has been deleted.");
+//            }
+        } catch (ClientResponseException $e) {
+            if ($e->getCode() === 404) {
+                $this->logWriter->errorMessage("The document with id 'my_id' does not exist.");
+            } else {
+                $this->logWriter->errorMessage("ClientResponseException: {$e->getMessage()}");
+                exit(1);
+            }
+        } catch (ServerResponseException $e) {
+            $this->logWriter->errorMessage("ServerResponseException: {$e->getMessage()}");
+            exit(1);
+        } catch (Exception $e) {
+            $this->logWriter->errorMessage("Exception: {$e->getMessage()}");
             exit(1);
         }
     }
