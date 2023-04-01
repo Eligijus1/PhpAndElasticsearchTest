46a3eb8c669d92a3de1f0d42de0d05e005dcb0de
Eligijus Stugys <eligijus.stugys@gmail.com>
2023-04-01 15:58:56 +0300

    Initial

--- src/CliCommands/TestDataTransferToElasticsearch.php
+++ src/CliCommands/TestDataTransferToElasticsearch.php
@@ -2,6 +2,7 @@
 
 namespace CliCommands;
 
+use Elastic\Elasticsearch\Client;
 use Elastic\Elasticsearch\ClientBuilder;
 use Elastic\Elasticsearch\Exception\AuthenticationException;
 use Elastic\Elasticsearch\Exception\ClientResponseException;
@@ -11,17 +12,23 @@ use Log\Writer;
 class TestDataTransferToElasticsearch implements CliCommandInterface
 {
     private Writer $logWriter;
-
     private array $env;
+    private Client $elasticClient;
 
     /**
      * @param Writer $logWriter
      * @param array $env
+     * @throws AuthenticationException
      */
     public function __construct(Writer $logWriter, array $env)
     {
         $this->logWriter = $logWriter;
         $this->env = $env;
+
+        $this->elasticClient = ClientBuilder::create()
+            ->setElasticCloudId($this->env['ELASTIC_CLOUD_WEB_UI_CLOUD_ID'])
+            ->setApiKey($this->env['ELASTIC_CLOUD_WEB_UI_API_KEY'])
+            ->build();
     }
 
     /**
@@ -33,15 +40,8 @@ class TestDataTransferToElasticsearch implements CliCommandInterface
     {
         $this->logWriter->debugMessage("execute method started.");
 
-//        $this->logWriter->debugMessage($this->env['ELASTIC_CLOUD_WEB_UI_CLOUD_ID']);
-//        $this->logWriter->debugMessage($this->env['ELASTIC_CLOUD_WEB_UI_API_KEY']);
-
-        $elasticClient = ClientBuilder::create()->setElasticCloudId($this->env['ELASTIC_CLOUD_WEB_UI_CLOUD_ID'])
-            ->setApiKey($this->env['ELASTIC_CLOUD_WEB_UI_API_KEY'])
-            ->build();
-
         try {
-            $response = $elasticClient->info();
+            $response = $this->elasticClient->info();
             $this->logWriter->debugMessage("Status code: {$response->getStatusCode()}");
         } catch (ClientResponseException $e) {
             $this->logWriter->errorMessage("ClientResponseException: {$e->getMessage()}");
@@ -53,5 +53,11 @@ class TestDataTransferToElasticsearch implements CliCommandInterface
     }
 }
 /*
-PHP Fatal error:  Uncaught GuzzleHttp\Exception\RequestException: cURL error 60: SSL certificate problem: self-signed certificate in certificate chain
- */
+2023-04-01 12:20:24 ERROR ClientResponseException:
+401 Unauthorized: {"error":{"root_cause":[{"type":"security_exception","reason":"unable to authenticate with provided credentials and anonymous access
+is not allowed for this request","additional_unsuccessful_credentials":"API key: unable to find apikey with id ZW6nPIcBf-4jny-WMBds",
+"header":{"WWW-Authenticate":["Basic realm=\"security\" charset=\"UTF-8\"","Bearer realm=\"security\"","ApiKey"]}}],
+"type":"security_exception","reason":"unable to authenticate with provided credentials and anonymous access is not
+ allowed for this request","additional_unsuccessful_credentials":"API key:
+unable to find apikey with id ZW6nPIcBf-4jny-WMBds","header":{"WWW-Authenticate":["Basic realm=\"security\" charset=\"UTF-8\"","Bearer realm=\"security\"","ApiKey"]}},"status":401}
+*/
